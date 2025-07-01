0dc44f588da5be39652767a2ae472790eaa6b21b
Raja Mohammad S <external.raja.mohammad@sky.de>
2025-07-01 21:42:02 +0530

    updated account

--- modules/durable_function/main.tf
+++ modules/durable_function/main.tf
@@ -1,29 +1,25 @@
 
 resource "azurerm_storage_account" "function_storage" {
-  name                     = "${var.project_name}funcstore"
+  name                     = "${replace(var.project_name, "-", "")}funcstore"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "${var.project_name}-plan"
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "FunctionApp"
-  reserved            = true
-  sku {
-    tier = "ElasticPremium"
-    size = "EP1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
 resource "azurerm_linux_function_app" "function" {
   name                       = "${var.project_name}-func"
   location                   = var.location
   resource_group_name        = var.resource_group_name
-  service_plan_id            = azurerm_app_service_plan.plan.id
+  service_plan_id            = azurerm_service_plan.plan.id
   storage_account_name       = azurerm_storage_account.function_storage.name
   storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
   functions_extension_version = "~4"
